a0c02b57ac71dcbef55f1477512097631cde8e12
Aji2324 <ajithece23@gmail.com>
2021-08-11 11:52:16 +0530

    push first

---
+++ src/org/sample/Constructor.java
@@ -0,0 +1,24 @@
+package org.sample;
+
+public class Constructor {
+	
+	public Constructor() {
+		this("Ajith", 2354);
+		
+		System.out.println("default Constructor1");
+		
+	}
+	
+	public Constructor(String name, int empId) {
+	
+		System.out.println(""+name+"\n"+empId);
+	}
+	
+	
+				
+		
+		
+		
+	}
+
+
---
+++ src/org/sample/ThisAndSuper.java
@@ -0,0 +1,30 @@
+package org.sample;
+
+public class ThisAndSuper extends Constructor {
+
+	public ThisAndSuper(String name){
+	 
+		System.out.println("name is:");
+	}
+	public ThisAndSuper() {
+		this("reyehy");
+		System.out.println("default");
+	}
+		public static void main(String[] args) {
+ThisAndSuper g=new ThisAndSuper();			
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
